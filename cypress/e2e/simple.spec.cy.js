e9956769150d72d90bdc879d2cdbae2244493ea8
Lucija Cabai <lucija.cabai@gmail.com>
2022-10-03 12:19:34 +0200

    run exapmle tests

--- cypress/e2e/simple.spec.cy.js
+++ cypress/e2e/simple.spec.cy.js
@@ -1,5 +1,11 @@
 describe('My first simple test', () => {
-    it("True should be True", () => {
+    it("True should be true", () => {
         expect(true).to.equal(true)
+    })
+    it("True should not be false", () => {
+        expect(true).to.not.equal(false)
+    })
+    it("5 is 5", () => {
+        expect(5).to.equal(5)
     })
 })
