0844e8c69f83a18cce3e5ae98c3fe683b844fc86
Lucija Cabai <lucija.cabai@gmail.com>
2022-10-03 15:17:45 +0200

    load url

--- cypress/e2e/load_url.spec.cy.js
+++ cypress/e2e/load_url.spec.cy.js
@@ -1,5 +1,25 @@
 describe('Browser Actions', () => {
-    it('should load correct url', () => {
-        cy.visit("http://google.com/")
+    it('load url', () => {
+        cy.visit("http://example.com/", { timeout: 20000 })
+    })
+
+    it("check url", () => {
+        cy.url().should("include", "example.com")
+    })
+
+    it("check if h1 is not null", () => {
+        cy.get("h1").isNotNull
+    })
+
+    it("should wait for 3 seconds", () => {
+        cy.wait(3000)
+    })
+
+    it("should pause", () => {
+        cy.pause()
+    })
+
+    it("check if h1 is visible", () => {
+        cy.get("h1").should("be.visible")
     })
 })
