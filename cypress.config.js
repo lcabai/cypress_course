8e7dc60c66bc42147583895ea09b2d50a08d5105
Lucija Cabai <lucija.cabai@gmail.com>
2022-10-04 17:07:05 +0200

    cypress.json is outdated, placed in cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    video: false,
+    viewportHeight: 1080,
+    viewportWidth: 1980,
+    watchForFileChanges: false
   },
 });
