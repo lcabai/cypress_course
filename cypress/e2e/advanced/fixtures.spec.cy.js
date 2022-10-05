c5f291f9b918fd32fa069d6aedc278228b5afb6d
Lucija Cabai <lucija.cabai@gmail.com>
2022-10-05 08:30:26 +0200

    fixtures

--- cypress/e2e/advanced/fixtures.spec.cy.js
+++ cypress/e2e/advanced/fixtures.spec.cy.js
@@ -3,10 +3,10 @@ describe('Login with fixture', () => {
         cy.visit("http://zero.webappsecurity.com/login.html")
 
         cy.fixture("user").then((user) => {
-            const username = user.username
-            const password = user.password
-            cy.get("#user_login").type(username)
-            cy.get("#user_password").type(password)
+            // const username = user.username
+            // const password = user.password
+            cy.get("#user_login").type(user.username)
+            cy.get("#user_password").type(user.password)
         })
 
         cy.contains("Sign in").click()
