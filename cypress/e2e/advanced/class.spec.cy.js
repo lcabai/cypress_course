e2ca02ea7add93ac2efa95e5e329c7cacdb85498
Lucija Cabai <lucija.cabai@gmail.com>
2022-10-05 14:17:30 +0200

    before and after hooks

--- cypress/e2e/advanced/class.spec.cy.js
+++ cypress/e2e/advanced/class.spec.cy.js
@@ -17,10 +17,28 @@ class HomePage extends BasePage {
 }
 
 describe('Abstraction with classes', () => {
-    it("should scroll down and up on page", () => {
+    before(function () {
+        // runs once before all tests
+        // setup test data or context, seed or reset db
         cy.viewport(1000, 660)
         HomePage.loadHomePage()
+    })
+
+    after(function () {
+        // runs once after all tests
+        // test clean up (cookies, local storage)
+    })
+
+    beforeEach(function () {
+        // runs once before EACH test
         HomePage.wait(500)
+    })
+
+    afterEach(function () {
+        // runs once after EACH test
+    })
+
+    it("should scroll down and up on page", () => {
         HomePage.scrollToBottom()
         HomePage.wait(1000)
         HomePage.scrollToTop()
