f72af87f4f4d9633bb5538e5336489e1d29a26e9
Lucija Cabai <lucija.cabai@gmail.com>
2022-10-07 13:59:19 +0200

    filter transactions

---
+++ cypress-e2e/cypress/e2e/section4/filter_transactions.cy.js
@@ -0,0 +1,27 @@
+describe('Filter transactions', () => {
+
+    Cypress.Cookies.debug(true, { verbose: false })
+
+    before(() => {
+        cy.visit("")
+        cy.get("#signin_button").click()
+        cy.login()
+    })
+    beforeEach(() => {
+        Cypress.Cookies.preserveOnce('JSESSIONID')
+    })
+    after(() => {
+        cy.wait(5000)
+        cy.contains("username").click()
+        cy.get("#logout_link").click()
+    })
+    it("Should navigate to find transactions page", () => {
+        cy.get("#account_activity_tab").click()
+        cy.contains("Find Transactions").click()
+    })
+    it("should filter transactions", () => {
+        cy.get("#aa_fromAmount").type("200")
+        cy.get("#aa_toAmount").type("1000")
+        cy.get("button[type='submit']").click()
+    })
+})
