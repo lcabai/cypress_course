7dde32d041a6d277a555d86a53dca344d65c6b8b
Lucija Cabai <lucija.cabai@gmail.com>
2022-10-10 12:39:10 +0200

    LoginPage, Navbar component

---
+++ cypress-framework/cypress/page-objects/pages/LoginPage.js
@@ -0,0 +1,11 @@
+export default class LoginPage extends BasePage {
+    static login(username, password) {
+        cy.login(username, password)
+    }
+    static clickForgotPassword() {
+        cy.contains("Forgot your password ?").click()
+    }
+    static displayErrorMessage() {
+        cy.isVisible(".alert-error")
+    }
+}
