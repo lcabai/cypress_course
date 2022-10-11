c68ebd78ea9a9a6357c715b8ddfed888ce340dd6
Lucija Cabai <lucija.cabai@gmail.com>
2022-10-11 14:18:46 +0200

    setup new project

---
+++ cypress-dashboard/cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    watchForFilesChanges: false,
+    chromeWebSecurity: false,
+    viewportWidth: 1000,
+    viewportHeight: 600,
+    waitForAnimation: true,
+    defaultCommandTimeout: 5000,
+    pageLoadTimeout: 10000,
+    video: false,
+    failOnStatusCode: false
+  },
+});
