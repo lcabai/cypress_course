5f176a30cf45c16eda5822a0890390c394aa36b1
Lucija Cabai <lucija.cabai@gmail.com>
2022-10-11 12:04:33 +0200

    add multiple sizes and pages; change date

--- cypress-visual-tests/cypress/e2e/image.cy.js
+++ cypress-visual-tests/cypress/e2e/image.cy.js
@@ -1,6 +1,17 @@
+const pages = ["www.example.com"]
+
+const sizes = ["iphone-6", "ipad-2", [1280, 800]]
+
 describe('Visual Regression', () => {
-  it("my first visual regression test", () => {
-    cy.visit("www.example.com")
-    cy.matchImageSnapshot()
+  sizes.forEach(size => {
+    pages.forEach(page => {
+      it(`should match ${page} in resolution ${size}`, () => {
+        let currentTime = new Date(Date.UTC(2020, 1, 1)).getDate()
+        cy.clock(currentTime)
+        cy.setResolution(size)
+        cy.visit(page)
+        cy.matchImageSnapshot()
+      })
+    })
   })
 })
